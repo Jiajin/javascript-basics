ec60f7b70089dec0f8febde9b5c796438eb42ef1
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-04 15:56:41 +0800

    done map

--- 4-map.js
+++ 4-map.js
@@ -6,50 +6,45 @@ function convertToUpperCase(array) {
   // let answer = array.map(x => x.toUpperCase());
   // console.log(answer);
   // return answer;
-  return  array.map(x => x.toUpperCase());
+  return array.map((x) => x.toUpperCase());
 }
-
 
 // Assertions (do not change)
 assertEquals(convertToUpperCase(["hello"]), ["HELLO"]);
 assertEquals(convertToUpperCase(["hello", "gOOdBYE"]), ["HELLO", "GOODBYE"]);
 
-
 /* ---------------------- EXERCISE 1 ---------------------- */
 // define a function that multiplies all elements in an array by 10
 function multiplyBy10(array) {
-  return array.map(x => x * 10);
+  return array.map((x) => x * 10);
 }
-
 
 // Assertions (do not change)
 assertEquals(multiplyBy10([1, 2]), [10, 20]);
 assertEquals(multiplyBy10([3, 5, 7]), [30, 50, 70]);
 assertEquals(multiplyBy10([-1, 0, 1]), [-10, 0, 10]);
 
-
 /* ---------------------- EXERCISE 2 ---------------------- */
 // define a function that takes in an array objects and returns only an array of strings of the name field
 
 function onlyNames(array) {
-  return array.map(x => x.name);
-
+  return array.map((x) => x.name);
 }
 
 // Assertions (do not change)
 var input = [
   {
     name: "homer",
-    favorite: "donuts"
+    favorite: "donuts",
   },
   {
     name: "marge",
-    favorite: "family"
+    favorite: "family",
   },
   {
     name: "lisa",
-    favorite: "school"
-  }
+    favorite: "school",
+  },
 ];
 
 assertEquals(onlyNames(input), ["homer", "marge", "lisa"]);
@@ -57,9 +52,8 @@ assertEquals(onlyNames(input), ["homer", "marge", "lisa"]);
 /* ---------------------- EXERCISE 3 ---------------------- */
 // define a function that removes any non-vowel character from words in an array
 function keepOnlyVowels(array) {
-  return array.map(x => x.replace(/[bcdfghjklmnpqrstvwxyz]/ig,''));
+  return array.map((x) => x.replace(/[bcdfghjklmnpqrstvwxyz]/gi, ""));
 }
-
 
 // Assertions (do not change)
 assertEquals(keepOnlyVowels(["abc"]), ["a"]);
@@ -67,5 +61,5 @@ assertEquals(keepOnlyVowels(["exceptional"]), ["eeioa"]);
 assertEquals(keepOnlyVowels(["average", "exceptional", "amazing"]), [
   "aeae",
   "eeioa",
-  "aai"
+  "aai",
 ]);
